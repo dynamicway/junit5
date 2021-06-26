bbd5d0f1973443cfb4b69fd3946edee3dc7d72a0
bee <rkddbtjd123@gmail.com>
2021-06-26 16:25:49 +0900

    feat: create assert_equals method

--- src/test/java/me/study/junit5/AssertionsMethodTest.java
+++ src/test/java/me/study/junit5/AssertionsMethodTest.java
@@ -1,7 +1,27 @@
 package me.study.junit5;
 
+import me.study.junit5.model.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AssertionsMethodTest {
 
+    @Test
+    @DisplayName("Assert Equals")
+    void assert_equals() {
+        // given
+        User user1 = User.builder()
+                .id(1L)
+                .username("user1")
+                .password("pass1")
+                .build();
+
+        // when
+        String username = user1.getUsername();
+
+        // then
+        assertEquals(user1.getUsername(), username);
+    }
 }
