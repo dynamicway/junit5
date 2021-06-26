728b101e4609ca90b9a4f7b746ae6b2eb9490b9f
bee <rkddbtjd123@gmail.com>
2021-06-26 16:12:13 +0900

    feat: generate equals and hashCode method in User

--- src/main/java/me/study/junit5/model/User.java
+++ src/main/java/me/study/junit5/model/User.java
@@ -1,7 +1,27 @@
 package me.study.junit5.model;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Objects;
+
+@Getter
+@Builder
 public class User {
     private Long id;
-    private String userName;
+    private String username;
     private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(username, user.username) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password);
+    }
 }
